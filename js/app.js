29defcd62eddbb8b519d8f22a978b27052d91386
yamilzavala <zavalayamil9@gmail.com>
2024-06-02 08:14:37 -0300

    add smooth scroll

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const navBtn = document.querySelector("#nav-btn");
 const closeBtn = document.querySelector("#close-btn");
 const sidebar = document.querySelector("#sidebar");
 const date = document.querySelector("#date");
+
 // add fixed class to navbar
 window.addEventListener("scroll", function () {
   if (window.pageYOffset > 80) {
@@ -11,6 +12,7 @@ window.addEventListener("scroll", function () {
     navbar.classList.remove("navbar-fixed");
   }
 });
+
 // show sidebar
 navBtn.addEventListener("click", function () {
   sidebar.classList.add("show-sidebar");
@@ -18,5 +20,31 @@ navBtn.addEventListener("click", function () {
 closeBtn.addEventListener("click", function () {
   sidebar.classList.remove("show-sidebar");
 });
+
 // set year
-date.innerHTML = new Date().getFullYear();
+//date.innerHTML = new Date().getFullYear();
+
+
+
+// select links
+const links = document.getElementById("nav-links");
+
+// ********** smooth scroll ************
+// select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    sidebar.classList.remove("show-sidebar");
+
+    const id = e.target.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    let position = element.offsetTop - 80;
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+      behavior: "smooth"
+    });
+  });
+});
